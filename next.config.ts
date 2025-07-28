7147e7bf06fb39748430bf25b478a5f52d3b2d72
Bent Schöne <bent.schoene@fh-bielefeld.de>
2025-07-28 16:03:28 +0200

    Add webpack config to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { resolve } from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: NextConfig) => {
+    config.resolve.alias['@'] = resolve(__dirname, 'src');
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 };
 
 export default nextConfig;
