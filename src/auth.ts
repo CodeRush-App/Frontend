075830597b910fd4e97225cbc181ee3782c837ac
Bent Schöne <bent.schoene@fh-bielefeld.de>
2026-09-01 16:43:51 +0200

    Remove trustHost auth option
    * Remove unconditional trustHost auth option to prevent host-header injection style issues
    * Remove real domain set in AUTH_COOKIE_DOMAIN env var and leave it commented out to opt in when necessary
    * Update the comment for the AUTH_URL env variable accordingly to document the removed trustHost option
    * Update env example to include COOKIE_SAMESITE var

--- src/auth.ts
+++ src/auth.ts
@@ -21,7 +21,6 @@ const sameSite = process.env.COOKIE_SAMESITE || "lax"
 
 export const authOptions: NextAuthConfig = {
     secret: process.env.AUTH_SECRET,
-    trustHost: true,
     session: {
         strategy: "jwt",
         maxAge: 30 * 24 * 60 * 60, // 30 days
